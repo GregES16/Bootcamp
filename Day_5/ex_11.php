0e1135cbfa3f3238b25f85cf1e3b30a2c9f38248
Greg <gregory.esteve@hotmail.com>
2023-04-19 11:05:37 +0200

    ex 13

--- Day_5/ex_11.php
+++ Day_5/ex_11.php
@@ -1,8 +1,12 @@
 <?php
 
+function print_array_with_key(array $my_array) {
+    foreach ($my_array as $key => $value) {
+        echo "$value : $key\n";
+    }
+}
 
-
-
+// print_array_with_key(["a" => "15", "b" => "3", "c" => "9"]);
 
 
 ?>
--- Day_5/ex_13.php
+++ Day_5/ex_13.php
@@ -1,5 +1,12 @@
 <?php
 
+function my_sort_files(array &$tab) {
+    foreach ($tab as $key => $value) {
+        echo sort($tab)."\n";
+    }
+}
+$t = ["a" => "baptiste", "b" => "emre", "c" => "test", "d" => "lumos"];
 
+my_sort_files($t);
 
 ?>
